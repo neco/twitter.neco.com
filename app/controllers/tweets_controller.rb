c0082a4ccdd5da495322ca3366537ee2b3783b34
Zack Ham <zackham@gmail.com>
2008-12-31 14:40:53 -0800

    updated tweet ordering

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,8 @@ class TweetsController < ApplicationController
   def index
     search_opts = params[:search] || {}
     search_opts[:per_page] ||= 10
-    search_opts[:order] = 'tweets.tweeted_at desc'
+    search_opts[:order_by] = :tweeted_at
+    search_opts[:order_as] = 'DESC'
     @search = Tweet.new_search(search_opts)
     @tweets, @tweets_count = @search.all, @search.count
   end
