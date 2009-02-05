6874fd5d57df6eb515fffd01555640ae67ba8569
Zack Ham <zackham@gmail.com>
2009-02-05 13:38:32 -0800

    added remember me

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,13 +11,31 @@ class TweetsController < ApplicationController
   def post
   end
   
+  def forget_me
+    session[:twitter_username] = nil
+    session[:twitter_password] = nil
+    redirect_to :action => 'post'
+  end
+  
   def create_tweet
     username, password, message = params[:username], params[:password], params[:message]
+    
+    if session[:twitter_username] && session[:twitter_password]
+      username, password = session[:twitter_username], session[:twitter_password]
+    end
+    
     twitter = Twitter.new(username, password)
     begin
       twitter.post(message)
       twitter_user = TwitterUser.find_by_screen_name(username)
       twitter_user.tweets.create :tweeted_at => Time.now, :tweet_text => message
+      
+      # only remember if post was successful
+      if params[:remember]
+        session[:twitter_username] = username
+        session[:twitter_password] = password
+      end
+      
       redirect_to :action => 'index'
     rescue
       @error = 'Bad username or password.'
--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -2,4 +2,8 @@ module TweetsHelper
   def parse_tweet(txt)
     txt.gsub(%r((http|https)://[^<>\s]+), %[<a href="\\0" target="_blank">\\0</a>])
   end
+  
+  def remembered?
+    session[:twitter_username] && session[:twitter_password]
+  end
 end
